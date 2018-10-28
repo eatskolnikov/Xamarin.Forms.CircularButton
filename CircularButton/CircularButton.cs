42955f62f8e2e708996706c120d9ce46866c2042
Enmanuel Toribio <enmanuel@torib.io>
2018-10-28 18:39:01 -0400

    First commit

---
+++ CircularButton/CircularButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CircularButton
+{
+    public class CircularButton : Button
+    {
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            CornerRadius = (int) width / 2;
+        }
+    }
+}
